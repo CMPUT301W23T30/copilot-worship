fb1708b548b435b7ef61c427612cad52afb7c638
Byao2 <byao2@ualberta.ca>
2023-03-01 22:05:42 -0700

    Added the QR camera reading, at March 1st

--- QRhunter/app/src/main/java/com/example/qrhunter/MainActivity.java
+++ QRhunter/app/src/main/java/com/example/qrhunter/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.qrhunter;
 
+import android.content.DialogInterface;
+import android.widget.Button;
 import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button scanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
         ImageView profileCircle = (ImageView) findViewById(R.id.default_profile_icon);
         //in the future if we want to add profile pictures
         profileCircle.setImageResource(R.drawable._icon__profile_circle_);
-
 
         FirebaseFirestore db;
 
@@ -34,5 +42,38 @@ public class MainActivity extends AppCompatActivity {
         collectionReference
                 .document("Lmao")
                 .set(data);
+
+        scanButton = findViewById(R.id.Scan_Button);
+        scanButton.setOnClickListener(v -> {
+            scanCode();
+        });
     }
+    private void scanCode() {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLaucher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barLaucher = registerForActivityResult(new ScanContract(), result->
+    {
+        if(result.getContents() !=null)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i)
+                {
+                    dialogInterface.dismiss();
+                }
+            }).show();
+        }
+    });
+
+
 }
