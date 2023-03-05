776573428219da42374a67b5c19b599a326f39b8
sean-madu <madusean2@gmail.com>
2023-03-04 23:54:41 -0700

    Resolve Merge conflicts

--- QRhunter/app/src/main/java/com/example/qrhunter/MainActivity.java
+++ QRhunter/app/src/main/java/com/example/qrhunter/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.qrhunter;
 
+import android.util.Log;
 import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 
@@ -20,19 +28,31 @@ public class MainActivity extends AppCompatActivity {
         //in the future if we want to add profile pictures
         profileCircle.setImageResource(R.drawable._icon__profile_circle_);
 
+        //Throwaway prototype to test database
+        Database db = new Database();
+        Player p = new Player();
+        db.addPlayer(p);
+        db.getPlayerFromUsername(p.getUsername())
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()) {
+                            /*
+                              Anything that needs the result of the query
+                              * must be in here, else since its async we might not be able to
+                              * access it...
+                             */
+                            //We are only expecting one return here
+                            for(QueryDocumentSnapshot doc : task.getResult()){
+                                System.out.println("username: " + doc.getId());
+                            }
+                        }
+                        else {
+                            Log.d("Database Error", "Could not retrieve" +
+                                    p.getUsername() + " Error: " + task.getException());
+                        }
+                    }
+                });
 
-        FirebaseFirestore db;
-
-        db = FirebaseFirestore.getInstance();
-
-        final CollectionReference collectionReference = db.collection("Cities");
-
-        HashMap<String, String> data = new HashMap<>();
-        data.put("Province Name", "lol");
-
-
-        collectionReference
-                .document("Lmao")
-                .set(data);
     }
 }
