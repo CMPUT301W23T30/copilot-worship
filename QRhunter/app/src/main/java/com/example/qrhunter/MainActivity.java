7dc2da20df9c14eeee0909366f5905e286383a7a
Byao2 <byao2@ualberta.ca>
2023-03-07 01:01:20 -0700

    Added finding photo path (URL?), at March 7th

--- QRhunter/app/src/main/java/com/example/qrhunter/MainActivity.java
+++ QRhunter/app/src/main/java/com/example/qrhunter/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.qrhunter;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,6 +17,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.core.content.PackageManagerCompat;
 
 import android.os.Bundle;
@@ -24,11 +28,19 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
+    final static int CAMERA_PERM_CODE = 101;
+    final static int CAMERA_REQUEST_CODE = 102;
     ImageView selectedImage;
+
+    String currentPhotoPath;
 
     Button scanButton;
     Button photoButton;
@@ -82,7 +94,6 @@ public class MainActivity extends AppCompatActivity {
         options.setOrientationLocked(true);
         options.setCaptureActivity(CaptureAct.class);
         barLaucher.launch(options);
-        Toast.makeText(this, "Make QR objects", Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -92,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
     {
         if(result.getContents() !=null)
         {
+            Toast.makeText(this, "make object", Toast.LENGTH_SHORT).show();
             AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
             builder.setTitle("Result");
             builder.setMessage(result.getContents());
@@ -115,36 +127,71 @@ public class MainActivity extends AppCompatActivity {
 
     private void askCameraPermissions() {
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.CAMERA}, 101);
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
         } else {
-            openCamera();
+            dispatchTakePictureIntent();
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 101) {
+        if (requestCode == CAMERA_PERM_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 //open camera
-                openCamera();
+                dispatchTakePictureIntent();
             } else {
                 Toast.makeText(this, "Camera Permission is required", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    private void openCamera() {
-        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(camera, 102);
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @NonNull Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 102) {
-            Bitmap image = (Bitmap)  data.getExtras().get("data");
-            selectedImage.setImageBitmap(image);
+        if (requestCode == Activity.RESULT_OK){
+            File f = new File(currentPhotoPath);
+            selectedImage.setImageURI(Uri.fromFile(f));
         }
     }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+    private void dispatchTakePictureIntent() {
+        Toast.makeText(this, "123", Toast.LENGTH_SHORT).show();
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Toast.makeText(this,"Error occurred while creating the File", Toast.LENGTH_SHORT).show();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
+    }
+
 }
