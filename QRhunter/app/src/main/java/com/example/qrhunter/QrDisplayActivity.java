70dcf590b3752968eec6566dec66942a9eecbe97
sean-madu <madusean2@gmail.com>
2023-03-08 12:04:37 -0700

    Add Qr Display

---
+++ QRhunter/app/src/main/java/com/example/qrhunter/QrDisplayActivity.java
@@ -0,0 +1,14 @@
+package com.example.qrhunter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QrDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_display);
+    }
+}
