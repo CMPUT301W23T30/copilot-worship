4561057196c59d2f232ee5b71abf30bdaeb6d6bb
BuildTools <unconfigured@null.spigotmc.org>
2023-03-13 01:49:07 -0700

    Test search functionality

--- QRhunter/app/src/androidTest/java/com/example/qrhunter/SearchPlayerTest.java
+++ QRhunter/app/src/androidTest/java/com/example/qrhunter/SearchPlayerTest.java
@@ -1,6 +1,8 @@
 package com.example.qrhunter;
 
 import android.app.Activity;
+import android.view.View;
+import android.widget.SearchView;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
@@ -9,6 +11,8 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  *
@@ -58,10 +62,32 @@ public class SearchPlayerTest {
      * @throws Exception
      */
     @Test
-    public void intentGoBack() throws Exception {
+    public void intentGoBackTest() throws Exception {
         solo.clickOnView(solo.getView(R.id.navbar_search_button));
         solo.assertCurrentActivity("Wrong Activity", SearchPlayerActivity.class);
         solo.goBack();
         solo.assertCurrentActivity("Go back didn't work", MainActivity.class);
+    }
+
+    /**
+     * Enter activity and search for player "Player-2"
+     * check for correct results
+     * @throws Exception
+     */
+    @Test
+    public void searchPlayerTest() throws Exception {
+        solo.clickOnView(solo.getView(R.id.navbar_search_button));
+        solo.assertCurrentActivity("Wrong Activity", SearchPlayerActivity.class);
+
+        // I can't seem to get click on view to work,
+        // in this case search is in a menu as an icon with no text so "" refers to
+        // the blank search item in the menu
+        solo.clickOnMenuItem("");
+        View searchView = solo.getCurrentViews(SearchView.class).get(0);
+        solo.clickOnView(searchView);
+        solo.enterText(0,"Player-2");
+
+        assertTrue(solo.searchText("Player-20"));
+        assertTrue(solo.searchText("Player-2"));
     }
 }
