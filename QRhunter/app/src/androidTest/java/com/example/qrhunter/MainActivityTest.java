1fa67aff35c406bfbecf9b6b77a1318bf4aec20b
sean-madu <madusean2@gmail.com>
2023-03-13 02:48:42 -0600

    Add Phone authentification Test

--- QRhunter/app/src/androidTest/java/com/example/qrhunter/MainActivityTest.java
+++ QRhunter/app/src/androidTest/java/com/example/qrhunter/MainActivityTest.java
@@ -1,5 +1,12 @@
 package com.example.qrhunter;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+
+import android.app.Activity;
+import android.widget.TextView;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
@@ -9,6 +16,7 @@ import com.robotium.solo.Solo;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -32,7 +40,33 @@ public class MainActivityTest {
         solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
     }
 
-    
+
+    /**
+     * Gets the Activity
+     * @throws Exception
+     */
+    @Test
+    public void start() throws Exception{
+        Activity activity = rule.getActivity();
+    }
+
+    /**
+     * Checks the user story that the phone authenticates the user
+     *
+     * TODO write a better test to show that the username is unique for new players
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testPhoneAuthentification() throws Exception{
+
+        TextView userText = (TextView) solo.getView(R.id.user_page_user_name);
+        assertNotEquals("",userText.getText());
+    }
+
+
+
+
     /**
      * Close activity after each test
      * @throws Exception
