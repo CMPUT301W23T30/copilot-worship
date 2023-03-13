ca13957df11c790c767c5f4668b5985fe5ab2950
sean-madu <madusean2@gmail.com>
2023-03-13 02:19:44 -0600

    Add MainActivityTest file

---
+++ QRhunter/app/src/androidTest/java/com/example/qrhunter/MainActivityTest.java
@@ -0,0 +1,45 @@
+package com.example.qrhunter;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ * Intent Test Class for MainActivity. Tests are written using Robotium
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    private Solo solo;
+    @Rule
+    public ActivityTestRule<MainActivity> rule =
+            new ActivityTestRule<>(MainActivity.class, true, true);
+    /**
+     * Runs before all tests and creates solo instance.
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception{
+
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+    }
+
+    
+    /**
+     * Close activity after each test
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
+    }
+
+}
