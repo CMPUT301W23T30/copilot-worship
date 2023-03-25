14df1830030db5802589b20b85806a0208b5d59e
maarijwastaken <105754222+maarijwastaken@users.noreply.github.com>
2023-03-25 15:16:04 -0600

    Changed canvas size, centered monster, tinkered with coordinate values.
    Image Rep part shouldn't need any more tinkering

--- QRhunter/app/src/main/java/com/example/qrhunter/CharacterImage.java
+++ QRhunter/app/src/main/java/com/example/qrhunter/CharacterImage.java
@@ -62,21 +62,22 @@ public class CharacterImage {
 
     public Bitmap getCharacterImage() {
         // Create a new bitmap with same dimensions as body bitmap
-        int width = 550;
-        int height = 550;
+        int width = 450;
+        int height = 450;
         int radius = 120;
         Bitmap characterImage = Bitmap.createBitmap(width,height,Bitmap.Config.ARGB_8888);
 
         // Get canvas for new bitmap
         Canvas canvas=new Canvas(characterImage);
+        canvas.drawColor(Color.LTGRAY);
 
         // Draw bitmaps onto canvas in desired order and positions
-        canvas.drawBitmap(body,radius,radius + radius/2 + hat.getHeight()/4,null);
-        canvas.drawBitmap(arms,radius + arms.getWidth()/2,radius + height/2-arms.getHeight()/2,null);
-        canvas.drawBitmap(legs,radius + radius/4,radius + 2*radius,null);
-        canvas.drawBitmap(eyes,radius + radius/4,radius + radius/2 + radius/6,null);
-        canvas.drawBitmap(mouth, radius + radius/4,radius + radius + hat.getHeight()/2,null);
-        canvas.drawBitmap(hat,radius + radius/4,radius-hat.getHeight()/2,null);
+        canvas.drawBitmap(body,radius - radius/8,radius/2 + hat.getHeight()/4,null);
+        canvas.drawBitmap(arms,radius + arms.getWidth()/2 - radius/8,2*radius,null);
+        canvas.drawBitmap(legs,radius + radius/7,2*radius,null);
+        canvas.drawBitmap(eyes,radius + radius/7,radius/2 + radius/5,null);
+        canvas.drawBitmap(mouth, radius + radius/7,radius + 2*legs.getHeight()/3,null);
+        canvas.drawBitmap(hat,radius + radius/7,hat.getHeight() - radius + hat.getHeight()/4,null);
 
         return characterImage;
     }
