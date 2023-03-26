4dea88648b4e722343b9a4d5b07e446814cb2d8c
Christine Bao <59641967+clbao@users.noreply.github.com>
2023-03-25 19:57:45 -0600

    Revert "Maps"

--- QRhunter/app/src/main/java/com/example/qrhunter/MapsActivity.java
+++ QRhunter/app/src/main/java/com/example/qrhunter/MapsActivity.java
@@ -228,8 +228,6 @@ public class MapsActivity extends FragmentActivity
                                                 if (distance <= visibleRadius) {
                                                     LatLng qrLocation = new LatLng(qrCode.getLocation().getLatitude(), qrCode.getLocation().getLongitude());
                                                     Marker marker = mMap.addMarker(new MarkerOptions().position(qrLocation).title(qrCode.getName()));
-                                                    String hash = qrCode.getHash();
-                                                    marker.setTag(hash);
                                                     markerList.add(marker);
                                                 }
                                             }
@@ -337,12 +335,10 @@ public class MapsActivity extends FragmentActivity
      */
     // When clicking on a marker, navigate to the QR code's detailed page
     public boolean onMarkerClick(Marker marker) {
-        // Get the QR code's hash from the marker's tag
-        String hash = (String) marker.getTag();
+        // Get the QR code's hash from the marker's title
+        String hash = marker.getTitle();
         // Navigate to the QR code's detailed page
         Intent intent = new Intent(this, QrDisplayActivity.class);
-        Bundle b = new Bundle();
-        b.putString("hash", hash);
         intent.putExtra("hash", hash);
         startActivity(intent);
         return false;
