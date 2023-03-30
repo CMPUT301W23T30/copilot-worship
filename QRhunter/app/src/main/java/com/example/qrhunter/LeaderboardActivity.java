6967ab07680a22ca7842c73957800c848178918e
sean-madu <madusean2@gmail.com>
2023-03-29 18:10:26 -0600

    Add ability to store and retrieve leaderboard

--- QRhunter/app/src/main/java/com/example/qrhunter/LeaderboardActivity.java
+++ QRhunter/app/src/main/java/com/example/qrhunter/LeaderboardActivity.java
@@ -18,6 +18,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Class that represents the leaderboard
+ */
 public class LeaderboardActivity extends AppCompatActivity implements LeaderboardAdapter.OnItemClickListener {
     private RecyclerView recyclerView;
     private LeaderboardAdapter adapter;
@@ -36,8 +39,8 @@ public class LeaderboardActivity extends AppCompatActivity implements Leaderboar
     private String thirdUsernameStr;
 
 
-    private SharedPreferences settings = getSharedPreferences("LocalLeaderboard", 0);
-    private SharedPreferences.Editor editor = settings.edit();
+    private SharedPreferences settings;
+    private SharedPreferences.Editor editor ;
 
 
     public void storeTop10(List<DocumentSnapshot> list){
@@ -46,32 +49,88 @@ public class LeaderboardActivity extends AppCompatActivity implements Leaderboar
         for(int i = 0; i < 10 || i < list.size(); i++){
             //We can store a set easily so we use the format i-username-score
             //Where i is the number of the player
-            top10.add( i + list.get(1).get("username").toString() + "-"
-                    + list.get(0).get("totalScore").toString());
+            String nullFlag = "\u0000";
+            top10.add( i + nullFlag + list.get(i).get("username").toString() + nullFlag
+                    + list.get(i).get("totalScore").toString());
         }
         editor.putStringSet("localTop10", top10);
         editor.putBoolean("playersSaved", true);
+        editor.commit();
     }
 
     public List<LeaderboardModel> getTop10(){
         List<LeaderboardModel> userList = new ArrayList<>();
         Set<String> top10 = new HashSet<>();
-        settings.getStringSet("localTop10", top10);
-        String[] topList = (String[]) top10.stream().sorted().toArray();
-        for (String formattedUser : topList) {
-            String username = formattedUser.substring(1, formattedUser.indexOf("-") + 1);
-            String score = formattedUser.substring(formattedUser.indexOf("-") + 1);
+        top10 = settings.getStringSet("localTop10", top10);
+        Object[] topList = top10.stream().sorted().toArray();
+        String nullFlag = "\u0000";
+        for (Object formattedObject : topList) {
+            String formattedUser = (String) formattedObject;
+            System.out.println(formattedUser);
+            //Cut off first terminator that stored position
+            formattedUser = formattedUser.substring(formattedUser.indexOf(nullFlag) + 1);
+            String username = formattedUser.substring(0, formattedUser.indexOf(nullFlag));
+            String score = formattedUser.substring(formattedUser.indexOf(nullFlag) + 1);
+            System.out.println(username + ":  - :" + score);
             LeaderboardModel user = new LeaderboardModel(username, Integer.valueOf(score));
             userList.add(user);
         }
         return userList;
     }
 
+    public void displayLeaderboardQuery(List<DocumentSnapshot> list){
+        // extract the top 3
+        DocumentSnapshot d1 = list.get(0);
+        firstUsernameStr = d1.get("username").toString();
+        firstUsername.setText(firstUsernameStr);
+        firstScore.setText(d1.get("totalScore").toString());
+        list.remove(0);
+        d1 = list.get(0);
+        secondUsernameStr = d1.get("username").toString();
+        secondUsername.setText(secondUsernameStr);
+        secondScore.setText(d1.get("totalScore").toString());
+        list.remove(0);
+        d1 = list.get(0);
+        thirdUsernameStr = d1.get("username").toString();
+        thirdUsername.setText(thirdUsernameStr);
+        thirdScore.setText(d1.get("totalScore").toString());
+        list.remove(0);
+
+        for (DocumentSnapshot d : list) {
+
+            LeaderboardModel user = new LeaderboardModel(d.get("username").toString(),Integer.parseInt(d.get("totalScore").toString()));
+
+            // after getting data from Firebase we are
+            // storing that data in our array list
+            userList.add(user);
+        }
+        adapter = new LeaderboardAdapter(userList, LeaderboardActivity.this, LeaderboardActivity.this::OnItemClick);
+        recyclerView.setAdapter(adapter);
+    }
+
+    public void displayLeaderboardSaved(List<LeaderboardModel> userList){
+        firstUsernameStr = userList.get(0).getUsername();
+        firstScore.setText(userList.get(0).getTotalScore().toString());
+        firstUsername.setText(firstUsernameStr);
+        secondUsernameStr = userList.get(1).getUsername();
+        secondScore.setText(userList.get(1).getTotalScore().toString());
+        secondUsername.setText(secondUsernameStr);
+        thirdUsernameStr = userList.get(3).getUsername();
+        thirdUsername.setText(thirdUsernameStr);
+        thirdScore.setText(userList.get(2).getTotalScore().toString());
+        userList.remove(0);
+        userList.remove(1);
+        userList.remove(2);
+        adapter = new LeaderboardAdapter(userList, LeaderboardActivity.this, LeaderboardActivity.this::OnItemClick);
+        recyclerView.setAdapter(adapter);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
 
+        settings = getSharedPreferences("LocalLeaderboard", 0);
+        editor = settings.edit();
         recyclerView = (RecyclerView) findViewById(R.id.leaderboard);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -83,44 +142,26 @@ public class LeaderboardActivity extends AppCompatActivity implements Leaderboar
         thirdUsername = findViewById(R.id.third_place_name);
         thirdScore = findViewById(R.id.third_place_score);
 
+        Boolean saved;
+        saved = settings.getBoolean("playersSaved", false);
 
-        
-
-        db.getPlayerCollectionTotalScore()
-                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                    @Override
-                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
-                        storeTop10(list);
-                        // extract the top 3
-                        DocumentSnapshot d1 = list.get(0);
-                        firstUsernameStr = d1.get("username").toString();
-                        firstUsername.setText(firstUsernameStr);
-                        firstScore.setText(d1.get("totalScore").toString());
-                        list.remove(0);
-                        d1 = list.get(0);
-                        secondUsernameStr = d1.get("username").toString();
-                        secondUsername.setText(secondUsernameStr);
-                        secondScore.setText(d1.get("totalScore").toString());
-                        list.remove(0);
-                        d1 = list.get(0);
-                        thirdUsernameStr = d1.get("username").toString();
-                        thirdUsername.setText(thirdUsernameStr);
-                        thirdScore.setText(d1.get("totalScore").toString());
-                        list.remove(0);
-
-                        for (DocumentSnapshot d : list) {
-
-                            LeaderboardModel user = new LeaderboardModel(d.get("username").toString(),Integer.parseInt(d.get("totalScore").toString()));
-
-                            // after getting data from Firebase we are
-                            // storing that data in our array list
-                            userList.add(user);
+        System.out.println(saved);
+        if(saved){
+            displayLeaderboardSaved(getTop10());
+            System.out.println("Gotten locally");
+        }
+        else{
+            db.getPlayerCollectionTotalScore()
+                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                        @Override
+                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                            List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                            storeTop10(list);
+                            displayLeaderboardQuery(list);
                         }
-                        adapter = new LeaderboardAdapter(userList, LeaderboardActivity.this, LeaderboardActivity.this::OnItemClick);
-                        recyclerView.setAdapter(adapter);
-                    }
-                });
+                    });
+        }
+
     }
 
     public void onClickFirst(View view) {
