7e3e50e8ca9326732df62e86556efe5d2ed6782d
maarijwastaken <105754222+maarijwastaken@users.noreply.github.com>
2023-04-03 13:52:34 -0600

    Create HasherAndScoreTest.java

---
+++ QRhunter/app/src/androidTest/java/com/example/qrhunter/HasherAndScoreTest.java
@@ -0,0 +1,29 @@
+package com.example.qrhunter;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.Before;
+import org.junit.Rule;
+
+public class HasherAndScoreTest {
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance.
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+    }
+    /**
+     * Gets the Activity
+     * @throws Exception
+     */
+}
