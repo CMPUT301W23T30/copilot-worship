ae6249ff119824a7f74b66bd9b9561ecc8d9f6a9
maarijwastaken <105754222+maarijwastaken@users.noreply.github.com>
2023-04-03 02:56:46 -0600

    Added lots of new tests for Add/Edit Player

--- QRhunter/app/src/androidTest/java/com/example/qrhunter/AddPlayerActivityTest.java
+++ QRhunter/app/src/androidTest/java/com/example/qrhunter/AddPlayerActivityTest.java
@@ -1,7 +1,10 @@
 package com.example.qrhunter;
 
+import static org.junit.Assert.assertEquals;
+
 import android.app.Activity;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -42,18 +45,68 @@ public class AddPlayerActivityTest {
     public void testAddPlayerActivityOpened() {
         solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
         solo.clickOnView(solo.getView(R.id.menu_add_player_button));
+        solo.waitForActivity(AddPlayerActivity.class, 2000);
         solo.assertCurrentActivity("Wrong Activity", AddPlayerActivity.class);
+    }
+
+    @Test
+    public void testAddPlayerActivityConditionChecks() {
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.clickOnView(solo.getView(R.id.menu_add_player_button));
+        solo.waitForActivity(AddPlayerActivity.class, 2000);
+        solo.assertCurrentActivity("Wrong Activity", AddPlayerActivity.class);
+
+        TextView errorText = (TextView) solo.getView(R.id.errorText);
+
+        solo.enterText((EditText) solo.getView(R.id.usernameEditText), "");
+        solo.clickOnView(solo.getView(R.id.submitButton));
+        assertEquals("Username must be between 1-20 Characters", errorText.getText().toString());
+        solo.enterText((EditText) solo.getView(R.id.usernameEditText), "test1234567890987654321123");
+        solo.clickOnView(solo.getView(R.id.submitButton));
+        assertEquals("Username must be between 1-20 Characters", errorText.getText().toString());
+        solo.enterText((EditText) solo.getView(R.id.usernameEditText), "test");
+
+        solo.enterText((EditText) solo.getView(R.id.emailEditText), "");
+        solo.clickOnView(solo.getView(R.id.submitButton));
+        assertEquals("You need an email silly!", errorText.getText().toString());
+        solo.enterText((EditText) solo.getView(R.id.emailEditText), "test@test.com");
+
+        solo.enterText((EditText) solo.getView(R.id.numberEditText), "");
+        solo.clickOnView(solo.getView(R.id.submitButton));
+        assertEquals("Forgetting something?", errorText.getText().toString());
+        solo.enterText((EditText) solo.getView(R.id.numberEditText), "1234567890");
+
+        solo.clickOnView(solo.getView(R.id.submitButton));
+        solo.waitForActivity(MainActivity.class, 2000);
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
     }
 
     @Test
     public void testAddPlayerActivitySubmit() {
         solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
         solo.clickOnView(solo.getView(R.id.menu_add_player_button));
+        solo.waitForActivity(AddPlayerActivity.class, 2000);
         solo.assertCurrentActivity("Wrong Activity", AddPlayerActivity.class);
+
         solo.enterText((EditText) solo.getView(R.id.usernameEditText), "test");
         solo.enterText((EditText) solo.getView(R.id.emailEditText), "test@test.com");
         solo.enterText((EditText) solo.getView(R.id.numberEditText), "1234567890");
+
         solo.clickOnView(solo.getView(R.id.submitButton));
+        solo.waitForActivity(MainActivity.class, 2000);
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+    }
+
+    @Test
+    public void testAddPlayerActivityCancel() {
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.clickOnView(solo.getView(R.id.menu_add_player_button));
+        solo.waitForActivity(AddPlayerActivity.class, 2000);
+        solo.assertCurrentActivity("Wrong Activity", AddPlayerActivity.class);
+
+        solo.goBack();
+        solo.waitForActivity(MainActivity.class, 2000);
         solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
     }
 }
