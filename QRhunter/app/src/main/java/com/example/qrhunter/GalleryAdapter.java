6ac53fd0712c2ce3eec939f2b3cc01dc8b34b0cd
bao_lehang <clbao@ualberta.ca>
2023-04-03 12:40:49 -0600

    I am once again fixing merges

--- QRhunter/app/src/main/java/com/example/qrhunter/GalleryAdapter.java
+++ QRhunter/app/src/main/java/com/example/qrhunter/GalleryAdapter.java
@@ -99,16 +99,15 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
 
         //TODO set QRCode Image
         name.setText(qrCode.getName());
-//        if (qrCode.getLocation().getLongitude() != 0){
-//            longitude.setText(df.format(qrCode.getLocation().getLongitude()));
-//            latitude.setText(df.format(qrCode.getLocation().getLatitude()));
-//        }
         //DELETE LATER
         if(qrCode.getName().length() > 16){
             name.setText(qrCode.getName().substring(0, 13) + "...");
         }
-        longitude.setText(df.format(qrCode.getLocation().getLongitude()));
-        latitude.setText(df.format(qrCode.getLocation().getLatitude()));
+
+        if (qrCode.getLocation().getLongitude() != 0) {
+            longitude.setText(df.format(qrCode.getLocation().getLongitude()));
+            latitude.setText(df.format(qrCode.getLocation().getLatitude()));
+        }
         
         score.setText(String.format("%d", qrCode.getScore()));
 
